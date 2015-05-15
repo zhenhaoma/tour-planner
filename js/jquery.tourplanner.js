c77e47c416df180c4257b29326bb859c3418dee3
MellowArpeggiation <georgewpaton@me.com>
2015-05-15 14:18:46 +1000

    Housekeeping
    JSLint hurt my feelints

--- js/jquery.tourplanner.js
+++ js/jquery.tourplanner.js
@@ -1,3 +1,11 @@
+// Global functions
+/*global google, console, Modernizr, $*/
+
+// Function list
+/*global initialize, geocodeAddress, setMapViewport, calculateRoute, generateTable*/
+
+"use strict";
+
 // Google Maps specific variable declaration
 var geocoder;
 var autocomplete;
@@ -16,14 +24,14 @@ var map;
 
 // Store the travel type
 var travelType = "walking";
-isLooping = false;
+var isLooping = false;
 
 // End of time (for cookie storage purposes)
-endOfTime = "expires=Fri, 31 Dec 9999 23:59:59 GMT;";
-immediate = "expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+var endOfTime = "expires=Fri, 31 Dec 9999 23:59:59 GMT;";
+var immediate = "expires=Thu, 01 Jan 1970 00:00:00 UTC;";
 
 // Preset Locations
-var australia = new google.maps.LatLng(-24.994167,134.866944)
+var australia = new google.maps.LatLng(-24.994167, 134.866944);
 var fullZoom = 4;
 var reasonableZoom = 10;
 
@@ -36,12 +44,20 @@ var bicycleLayer;
 // Variables of the current location set by asynchronous method 
 var currentLocationName;
 var currentLocation;
-var addedAttractionsArray = new Array();
+var addedAttractionsArray = [];
 
 
 function initialize() {
 	//Map Specifications
-	var mapProp = {
+    
+    // Function scope variables
+    var mapProp,
+        startLocationAutocomplete,
+        destinationLocationAutocomplete,
+        options,
+        input;
+    
+	mapProp = {
 		center: australia,
 		zoom: 4,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -52,25 +68,25 @@ function initialize() {
 					{ hue: "#2B167B" },
 					{ saturation: 0 }
 				]
-			},{
+			}, {
 				featureType: "road",
 				elementType: "geometry",
 				stylers: [
 					{ lightness: 60 },
 					{ visibility: "simplified" }
 				]
-			},{
+			}, {
 				elementType: "labels",
 				stylers: [
 					{ visibility: "off" }
 				]
-			},{
+			}, {
 				featureType: "road.highway",
 				elementType: "geometry",
 				stylers: [
 					{ hue: "#F05329" }
 				]
-			},{
+			}, {
 				featureType: "poi.attraction",
 				elementType: "geometry",
 				stylers: [
@@ -78,7 +94,7 @@ function initialize() {
 					{ saturation: 50 },
 					{ lightness: -10 }
 				]
-			},{
+			}, {
 				featureType: "poi.sports_complex",
 				elementType: "geometry",
 				stylers: [
@@ -86,32 +102,32 @@ function initialize() {
 					{ saturation: 50 },
 					{ lightness: -10 }
 				]
-			},{
+			}, {
 				featureType: "poi.park",
 				elementType: "geometry",
 				stylers: [
 					{ hue: "#08CA74" },
 					{ saturation: 50 }
 				]
-			},{
+			}, {
 				featureType: "poi",
 				elementType: "labels",
 				stylers: [
 					{ visibility: "on" }
 				]
-			},{
+			}, {
 				featureType: "poi",
 				elementType: "labels.text.stroke",
 				stylers: [
 					{ lightness: -100 }
 				]
-			},{
+			}, {
 				featureType: "poi",
 				elementType: "labels.text.fill",
 				stylers: [
 					{ lightness: 100 }
 				]
-			},{
+			}, {
 				featureType: "administrative",
 				elementType: "labels",
 				stylers: [
@@ -123,11 +139,11 @@ function initialize() {
 	
 	// Detect browser location support
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			userLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+		navigator.geolocation.getCurrentPosition(function (position) {
+			userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 			map.setCenter(userLocation);
 			map.setZoom(reasonableZoom);
-		}, function() {
+		}, function () {
 			console.log("Geolocation service failed");
 		});
 	} else {
@@ -135,26 +151,26 @@ function initialize() {
 	}
 		
 	//Add Map to Div
-	map = new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapProp);
 
 	//Initialise Geocoder object
 	geocoder = new google.maps.Geocoder();
 
 	// Initialising the autocomplete objects, restricting the search
 	// to geographical location types.
-	startLocationAutocomplete = new google.maps.places.Autocomplete((document.getElementById('start-location')),{ types: ['geocode'] });
-	destinationLocationAutocomplete = new google.maps.places.Autocomplete((document.getElementById('end-location')),{ types: ['geocode'] });
+	startLocationAutocomplete = new google.maps.places.Autocomplete((document.getElementById('start-location')), { types: ['geocode'] });
+	destinationLocationAutocomplete = new google.maps.places.Autocomplete((document.getElementById('end-location')), { types: ['geocode'] });
 
 
 	// When the user selects an address from the dropdown,
 	// populate the address fields in the form.
 	// start location autocomplete event
-	google.maps.event.addListener(startLocationAutocomplete, 'place_changed', function() {
+	google.maps.event.addListener(startLocationAutocomplete, 'place_changed', function () {
 		geocodeAddress(document.getElementById('start-location').value, 1);
 	});
 
 	// destination location autocomplete event
-	google.maps.event.addListener(destinationLocationAutocomplete, 'place_changed', function() {
+	google.maps.event.addListener(destinationLocationAutocomplete, 'place_changed', function () {
 		geocodeAddress(document.getElementById('end-location').value, 2);
 	});
 	
@@ -179,24 +195,24 @@ function initialize() {
 
 
 	//POPUPWINDOW: Autocomplete search filtering conditions set to only establishments within australia
-	var options = {
+	options = {
 		types: ['establishment'],
 		componentRestrictions: {country: "aus"}
 	};
 
 
 	//POPUPWINDOW: Textfield input
-	var input = document.getElementById('attraction-location');
+	input = document.getElementById('attraction-location');
 	
 					
 	// Initialising the autocomplete objects, restricting the search
 	// to geographical location types.
-	startLocationAutocomplete = new google.maps.places.Autocomplete(input,options);
+	startLocationAutocomplete = new google.maps.places.Autocomplete(input, options);
 		
 	// When the user selects an address from the dropdown,
 	// populate the address fields in the form.
 	//start location autocomplete event
-	google.maps.event.addListener(startLocationAutocomplete, 'place_changed', function() {
+	google.maps.event.addListener(startLocationAutocomplete, 'place_changed', function () {
 		currentLocationName = document.getElementById('attraction-location').value;
 		geocodeAddress(document.getElementById('attraction-location').value, 3);
 		
@@ -216,24 +232,24 @@ function initialize() {
 		draggable: true
 	});
 	
-	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="tooltip"]').tooltip();
 }
 
 //Takes the entered address and set the start and end location latitude and longitude
 //The assign variable being a 1 or a 2 determines whether the locations is the start or destination
 function geocodeAddress(location, assign) {
-	geocoder.geocode( { 'address': location}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
+	geocoder.geocode({ 'address': location }, function (results, status) {
+		if (status === google.maps.GeocoderStatus.OK) {
 			//global variable assignment
-			if (assign == 1) {
+			if (assign === 1) {
 				startLocation = results[0].geometry.location;
 				startMarker.setMap(map);
 				startMarker.setPosition(startLocation);
-			} else if (assign == 2) {
+			} else if (assign === 2) {
 				destinationLocation = results[0].geometry.location;
 				destinationMarker.setMap(map);
 				destinationMarker.setPosition(destinationLocation);
-			} else if (assign == 3) {
+			} else if (assign === 3) {
 				currentLocation = results[0].geometry.location;
 			}
 			
@@ -267,7 +283,7 @@ function setMapViewport(start, end) {
 }
 
 function addAttraction() {
-	var location = {name:currentLocationName, lat:currentLocation.lat(), lng:currentLocation.lng()};
+	var location = {name: currentLocationName, lat: currentLocation.lat(), lng: currentLocation.lng()};
 	addedAttractionsArray.push(location);
 
 	// Clears the attractions auto complete box 
@@ -279,22 +295,30 @@ function addAttraction() {
 
 
 function generateTable() {
-	var table = document.getElementById("attraction-table");
+    var table,
+        row,
+        locationNameCell,
+        travelInfoCell,
+        infoButtonCell,
+        closeButtonCell,
+        i;
+    
+	table = document.getElementById("attraction-table");
 
 	// Delete contents of table
 	table.innerHTML = "";
 	
-	for (i = 0; i < addedAttractionsArray.length; i++) {
-		var row = table.insertRow(-1);
+	for (i = 0; i < addedAttractionsArray.length; i += 1) {
+		row = table.insertRow(-1);
 
-		var locationNameCell = row.insertCell(0);
-		var travelInfoCell = row.insertCell(1);
-		var infoButtonCell = row.insertCell(2);
-		var closeButtonCell = row.insertCell(3);
+		locationNameCell = row.insertCell(0);
+		travelInfoCell = row.insertCell(1);
+		infoButtonCell = row.insertCell(2);
+		closeButtonCell = row.insertCell(3);
 
 		locationNameCell.innerHTML = addedAttractionsArray[i].name;
 		travelInfoCell.innerHTML = "00:00 0km";
-		infoButtonCell.innerHTML = "<span class='glyphicon glyphicon-info-sign'></span>"
+		infoButtonCell.innerHTML = "<span class='glyphicon glyphicon-info-sign'></span>";
 		closeButtonCell.innerHTML = "<button type='button' class='close' onclick='deleteAttraction(this)'>&times;</button>";
 	}
 }
@@ -306,7 +330,7 @@ function deleteAttraction(button) {
 	addedAttractionsArray.splice(row.index(), 1);
 	
 	generateTable();
-}	
+}
 
 function placeAttractions() {
 	
@@ -317,23 +341,12 @@ function shareRoute() {
 }
 
 function calculateRoute() {
-	if(travelType == "driving") {
-		
-	}
-	else if(travelType == "walking") {
-		
-	}
-	else if(travelType == "cycling") {
-		
-	}
-	else if(travelType == "transit") {
-		
-	}
+    
 }
 
 function setTravelType(originElement, newTravelType) {
 	travelType = newTravelType;
-	$("#travel-mode>.btn-group").each(function() {
+	$("#travel-mode>.btn-group").each(function () {
 		$(this).children().first().attr("class", "btn btn-default");
 	});
 	$(originElement).attr("class", "btn btn-success");
@@ -343,22 +356,22 @@ function setTravelType(originElement, newTravelType) {
 	trafficLayer.setMap(null);
 	
 	switch (travelType) {
-		case "transit":
-			transitLayer.setMap(map);
-			break;
-		case "cycling":
-			bicycleLayer.setMap(map);
-			break;
-		case "driving":
-			trafficLayer.setMap(map);
-			break;
+    case "transit":
+        transitLayer.setMap(map);
+        break;
+    case "cycling":
+        bicycleLayer.setMap(map);
+        break;
+    case "driving":
+        trafficLayer.setMap(map);
+        break;
 	}
 }
 
 function setRoundTrip(button) {
 	// Returns a string rather than a boolean
 	// ALSO is inverted
-	if ($(button).attr("aria-pressed") != "true") {
+	if ($(button).attr("aria-pressed") !== "true") {
 		isLooping = true;
 		$("#end-location").hide();
 		$(button).addClass("btn-success");
